71e54222a5127926581a449c957bb40b26b49655
vsofat <vsofat00@stuy.edu>
2018-10-26 08:58:13 -0400

    phase 2 complete

--- SuperArray.java
+++ SuperArray.java
@@ -4,12 +4,12 @@ public class SuperArray{
 	private int size;
   // constructor
 	public SuperArray(){
-		data = new String[10]; // couldn't clear replace this?
+		this.data = new String[10]; // couldn't clear replace this?
 	}
   // accessors
  public void clear(){
    size = 0; // using this.size would be the same thing here
-   data = new String[10];
+   this.data = new String[10];
    // Removes all of the elements from this list. You can just set size to 0 for this!
  }
  public int size(){
@@ -31,7 +31,7 @@ public boolean add(String str){
     resize();
   }
   else {
-    data[size] = str;
+    this.data[size] = str;
     size = size + 1;
   }
   return true;
@@ -39,16 +39,30 @@ public boolean add(String str){
   Returns true. Should resize the list before adding if there is no more space. */
 }
 public String toString(){
-String result = "[]"
+String result = "["
 for (int i = 0; i < this.data.length - 1; i++){
   result += data[x] + ", ";
 }
 if (size() > 0){
-  result += data[(size())-1];
+  result += data[(size())-1];}
+  result += "]";
   return result;
 }
   // Returns the data in the format: "[A, B, FISH, cat, Dog]" Without double quotes around the data elements.
 }
+public String toStringDebug() {
+    String result = "[";
+    for(int i = 0; i < data.length; i++) {
+      ans += this.data[i];
+      if (i != data.length - 1)) {
+        result += ", ";
+      }
+    }
+    result += "]";
+    return result;
+    // toString + null values for Debugging purposes
+  }
+
 public String get(int index){
   if (index < 0 || index >= size){
     return null;} // combine commented out code into one line
@@ -76,5 +90,25 @@ public String	set(int index, String value){
   /* Replaces the element at the specified position in this list with the specified element, returns the old element.
      Prints an error when index is out of range. */
 }
-//
+private void resize() {
+    String[] Ary = new String[this.size];
+    for(int i = 0; i < this.size; i++) {
+      Ary[i] = this.data[i];
+    }
+    this.data = new String[(this.size * 2)];
+    for(int i = 0; i < this.size; i++) {
+      this.data[i] = Ary[i];
+    }
+    /* This is a private method that you call when you need more space.
+       It will increase the capacity by making a new Array of Strings and copying the old one over. */
+  }
+public boolean contains(String target){
+  for (int i = 0; i < this.size; i++){
+    if (data[i].equals(target){
+      return true;
+    }  
+    // Returns true if this list contains the specified element.
+
+  }
+}
 }
